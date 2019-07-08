d93e8c1bcec9b7b0fa22d80496bfeac6d0f3e5e0
Leeyoujung1995 <lyj2597@naver.com>
2019-07-08 12:17:56 +0900

    bitcamp-java-application 08버전

---
+++ bitcamp-java-application/v08/src/main/java/com/eomcs/lms/Member.java
@@ -0,0 +1,15 @@
+package com.eomcs.lms;
+
+import java.sql.Date;
+
+public class Member {
+  
+  int num;
+  String name;
+  String email;
+  int password;
+  int tel;
+  String photo;
+  Date registeredDate;
+  
+}
