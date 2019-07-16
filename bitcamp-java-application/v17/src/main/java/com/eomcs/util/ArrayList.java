12ab227281b09a25d4c53201033af7d22f71a4c1
Leeyoujung1995 <lyj2597@naver.com>
2019-07-16 10:41:16 +0900

    bitcamp-java-application v17

---
+++ bitcamp-java-application/v17/src/main/java/com/eomcs/util/ArrayList.java
@@ -0,0 +1,39 @@
+package com.eomcs.util;
+
+import java.util.Arrays;
+
+public class ArrayList {
+  private static final int DEFAULT_Capacity =100;
+  
+  private Object[] list ;
+  private  int size = 0;
+  
+  public ArrayList() {
+   this(DEFAULT_Capacity);//생성자에서 다른 생성자를 호출할 수 있다.
+  }
+  
+  public ArrayList(int initialCapacity) {
+    if(initialCapacity < 50 || initialCapacity >10000)
+      list = new Object[DEFAULT_Capacity];
+    else
+      list = new Object[initialCapacity];
+  }
+  
+  public void add(Object obj) {
+    if(this.size == list.length) {
+      int oldCapacity = list.length;
+      int newCapacity = oldCapacity +(oldCapacity >> 1 );
+      list = Arrays.copyOf(this.list, newCapacity);
+    }
+    this.list[this.size++] =obj;
+  }
+  
+  public Object[] toArray() {
+    return Arrays.copyOf(this.list, this.size);  
+  }
+  
+  
+  
+  
+  
+}
