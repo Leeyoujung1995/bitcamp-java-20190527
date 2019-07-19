db800b6513fb9473dcecfa33473d7dd0af47411d
Leeyoujung1995 <lyj2597@naver.com>
2019-07-19 12:17:42 +0900

    bitcamp-java-application v23

---
+++ bitcamp-java-application/v23/src/main/java/com/eomcs/util/Node.java
@@ -0,0 +1,14 @@
+package com.eomcs.util;
+
+public class Node {
+  Object value;
+  Node next;
+  
+  public Node() {//기본생성자
+    
+  }
+  public Node(Object value) {
+    this.value =value;
+    
+  }
+}
