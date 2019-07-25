bb9d4c6d538f2f5ba5a8734086919ea4c1e9ab9b
Leeyoujung1995 <lyj2597@naver.com>
2019-07-25 14:42:49 +0900

    bicamp-java-application v28

---
+++ bitcamp-java-application/v28/src/main/java/com/eomcs/lms/Car.java
@@ -0,0 +1,17 @@
+package com.eomcs.lms;
+
+public class Car {
+  //static/class field
+  //-->클래스가 로딩될 때 Method Area에 자동 생성된다.
+  //-->클래스는 딱 한번만 로딩되기 때문에(중복으로 로딩되지 않는다)
+  //스태틱 필드도 딱 한번만 생성된다
+  static int count;
+  
+  
+  //instance field
+  //-->new 명령을 실행할 때 Heap에 생성된다.
+  //-->new 명
+  int no;
+  String model;
+  
+}
