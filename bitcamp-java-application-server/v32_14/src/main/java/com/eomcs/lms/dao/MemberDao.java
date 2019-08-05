0a4598802759f77fa157eb316bc145d0b2781767
Leeyoujung1995 <lyj2597@naver.com>
2019-08-05 14:44:46 +0900

    java 0805

---
+++ bitcamp-java-application-server/v32_14/src/main/java/com/eomcs/lms/dao/MemberDao.java
@@ -0,0 +1,32 @@
+package com.eomcs.lms.dao;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import com.eomcs.lms.dao.serial.AbstractDataSerializer;
+import com.eomcs.lms.domain.Member;
+
+public interface MemberDao  {
+
+
+
+  int insert(Member member) throws Exception ;
+
+  List<Member> findAll() throws Exception ;
+
+  Member findBy(int no) throws Exception ;
+
+  int update(Member member) throws Exception ;
+
+  int delete(int no) throws Exception ;
+
+
+}
+
+
+
+
+
+
+
+
