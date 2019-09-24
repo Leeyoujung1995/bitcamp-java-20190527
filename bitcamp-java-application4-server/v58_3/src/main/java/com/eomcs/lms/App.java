a71981512a05a2738c6e473d825d355ea33a863a
Leeyoujung1995 <lyj2597@naver.com>
2019-09-24 17:51:17 +0900

    java0924

---
+++ bitcamp-java-application4-server/v58_3/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,17 @@
+// v58_3 :Request Handler를 @RequestMapping애노테이션으로 표시하기
+package com.eomcs.lms;
+
+// 작업:
+//google reflections 라이브러리 추가
+//클래스에서 필드나 메서드등의 정보를 좀더 쉽게 뽑아주는 도구
+//PageController인터페이스 삭제
+//XxxController의 request handler에 @RequestMapping 애노테이션 붙이기
+//@ReqiestMapping이 붙은 메서드를 호출하도록 DispatcherServlet으로 변경
+public class App {
+}
+
+
+
+
+
+
---
+++ bitcamp-java-application4-server/v58_4/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,16 @@
+// v58_4 :CRUD 기능을 한 클래스로 묶는다.
+package com.eomcs.lms;
+
+// 작업:
+//BoardXxxController의 request handler를 BoardController로 묶는다
+//페이지 컨트롤러임을 명확하게 표시하기 위해 @Component 대신 @Controller로 표시한다.
+//RequestMappingHandler 생성
+//DispatcherServlet으로 변경
+public class App {
+}
+
+
+
+
+
+
